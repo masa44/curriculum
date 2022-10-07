5a5a87d82e3819f672d78320653e861ae018b8ee
masa44 <masayoshi.kotera@gmail.com>
2022-10-07 23:53:58 +0900

    ７章チェックテスト
    ６章は少し時間がかかりそうなので、７章を先に進めました。
    問１は、要求仕様？がよくわからなかったので、提示されている isEven() 関数を変更せずにそのまま実行する形で実装しました。

---
+++ 7/main.js
@@ -0,0 +1,32 @@
+// 問１
+let numbers = [2, 5, 12, 13, 15, 18, 22];
+//ここに答えを実装してください。↓↓↓
+let num = 0;
+for(let i = 0; i < numbers.length; i++) {
+	if(numbers[i] % 2 === 0) {
+		num = numbers[i];
+		isEven();
+	}
+}
+
+function isEven() {
+	console.log(num + 'は偶数です');
+}
+
+
+// 問２
+class Car {
+
+	constructor(gass, num) {
+		this.gass = gass;
+		this.num = num;
+	}
+
+	getNumGas() {
+		console.log(`ガソリンは${this.gass}です。ナンバーは${this.num}です`);
+	}
+};
+
+let testCar = new Car(20.5, "1234");
+testCar.getNumGas();
+
